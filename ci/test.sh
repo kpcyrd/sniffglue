debc60adc4865ccccf8b34cb9bc8cd830c41985c
kpcyrd <git@rxv.cc>
2017-10-10 01:27:46 +0200

    Travis: Test reading pcap works as user

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@ set -ex
 
 case "$BUILD_MODE" in
     release)
-        # skip
+        RUST_LOG=sniffglue target/$TARGET/release/sniffglue -r pcaps/SkypeIRC.pcap > /dev/null
         ;;
     *)
         cargo test --verbose --target="$TARGET"
