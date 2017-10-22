87b59e952be8937cf65427cec0069276b8607976
kpcyrd <git@rxv.cc>
2017-10-22 20:58:58 +0200

    Add env_logger

--- examples/read_packet.rs
+++ examples/read_packet.rs
@@ -1,9 +1,12 @@
 extern crate base64;
 extern crate sniffglue;
+extern crate env_logger;
 
 use std::env;
 
 fn main() {
+    env_logger::init().unwrap();
+
     for arg in env::args().skip(1) {
         let bytes = base64::decode(&arg).unwrap();
         println!("{:?}", bytes);
