0a755b59da6f33ef9503c4e20384a40068d09752
kpcyrd <git@rxv.cc>
2017-12-18 01:51:38 +0100

    Fix new i686 crosscompile

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -5,6 +5,9 @@ case "$1" in
     aarch64-unknown-linux-gnu)
         dpkg --add-architecture arm64
         ;;
+    i686-unknown-linux-gnu)
+        dpkg --add-architecture i386
+        ;;
 esac
 
 apt-get -q update
