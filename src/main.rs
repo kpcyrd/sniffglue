965cb33af8a4f5c65c57df4a632c8b44ac3a96e4
kpcyrd <git@rxv.cc>
2018-02-19 18:26:50 +0100

    Read version from cargo

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,7 @@ fn main() {
     sandbox::activate_stage1().expect("init sandbox stage1");
 
     let matches = App::new("sniffglue")
-        .version("0.4.1")
+        .version(env!("CARGO_PKG_VERSION"))
         .setting(AppSettings::ColoredHelp)
         .arg(Arg::with_name("promisc")
             .short("p")
