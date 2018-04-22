923f00004c5bc2e58f5d702d29768114d5ebfdcc
kpcyrd <git@rxv.cc>
2018-04-22 05:58:57 +0200

    Add clippy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![warn(unused_extern_crates)]
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
 extern crate pcap;
 #[macro_use] extern crate nom;
 extern crate pktparse;
