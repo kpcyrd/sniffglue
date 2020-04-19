8b1b12b422ac3a83594c5968f7473d87b08b3df7
kpcyrd <git@rxv.cc>
2020-04-19 15:58:16 +0200

    Port to nom 5

--- src/centrifuge/cjdns.rs
+++ src/centrifuge/cjdns.rs
@@ -1,27 +1,39 @@
 use crate::structs::{cjdns, CentrifugeError};
-use nom::be_u16;
+use nom::number::complete::be_u16;
+use nom::bytes::complete::{tag, take};
 
 const BEACON_PASSWORD_LEN: usize = 20;
 const BEACON_PUBKEY_LEN: usize = 32;
 
 
-named!(cjdns_eth_header<&[u8], cjdns::CjdnsEthPkt>, do_parse!(
-    _version:       tag!(b"\x00") >>
-    _zero:          tag!(b"\x00") >>
-    _length:        be_u16 >>
-    _fc00:          tag!(b"\xfc\x00") >>
-    _padding:       take!(2) >>
+fn cjdns_eth_header(input: &[u8]) -> nom::IResult<&[u8], cjdns::CjdnsEthPkt> {
+    let (input, (
+        _version,
+        _zero,
+        _length,
+        _fc00,
+        _padding,
+        version,
+        password,
+        pubkey,
+    )) = nom::sequence::tuple((
+        tag(b"\x00"),
+        tag(b"\x00"),
+        be_u16,
+        tag(b"\xfc\x00"),
+        take(2usize),
 
-    version:        be_u16 >>
-    password:       take!(BEACON_PASSWORD_LEN) >>
-    pubkey:         take!(BEACON_PUBKEY_LEN) >>
+        be_u16,
+        take(BEACON_PASSWORD_LEN),
+        take(BEACON_PUBKEY_LEN),
+    ))(input)?;
 
-    ({ cjdns::CjdnsEthPkt {
+    Ok((input, cjdns::CjdnsEthPkt {
         version,
         password: password.to_vec(),
         pubkey: pubkey.to_vec(),
-    } })
-));
+    }))
+}
 
 pub fn parse(remaining: &[u8]) -> Result<cjdns::CjdnsEthPkt, CentrifugeError> {
     if let Ok((remaining, cjdns_eth_hdr)) = cjdns_eth_header(remaining) {
