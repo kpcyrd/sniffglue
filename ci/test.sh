63e0ca87f44e26ba5f15f6295c32c8738fdb9f0f
kpcyrd <git@rxv.cc>
2020-09-30 03:29:59 +0200

    Move some tests to github actions

--- ci/test.sh
+++ ci/test.sh
@@ -5,9 +5,6 @@ case "$BUILD_MODE" in
     release)
         RUST_LOG=sniffglue target/$TARGET/release/sniffglue -r pcaps/SkypeIRC.pcap > /dev/null
         ;;
-    docker)
-        docker run -e RUST_LOG=sniffglue sniffglue --help
-        ;;
     boxxy)
         if ! cat ci/boxxy_stage0.txt | RUST_LOG=boxxy cargo run --example boxxy | grep -q 'cargo run --example boxxy'; then
             echo SANDOX ERROR: expected match
@@ -29,8 +26,5 @@ case "$BUILD_MODE" in
         ;;
     cross)
         # do not execute tests when cross compiling
-        ;;
-    *)
-        cargo test --verbose --target="$TARGET"
         ;;
 esac
